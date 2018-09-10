ee6bf6650c694fcaf630a5cb83d56c711c42132b
NathanClouseAX <Nathan.clouse@gmail.com>
2018-09-09 20:21:45 -0700

    initial commit

---
+++ AAX_AzureQueueLibrary/ReceiveAzureQueue.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+
+namespace AAX_AzureQueueLibrary
+{
+    public class ReceiveAzureQueue
+    {
+        QueueClient queueClient;
+        BrokeredMessage message;
+
+        public void connect(string connectionString)
+        {
+            queueClient = QueueClient.CreateFromConnectionString(connectionString, ReceiveMode.PeekLock);
+        }
+
+        public string readMessage()
+        {
+            message = queueClient.Receive();
+
+            string s = message.GetBody<String>();
+
+            return (s);
+        }
+
+        public void completeMessage()
+        {
+            queueClient.Complete(message.LockToken);
+        }
+
+        public void close()
+        {
+            queueClient.Close();
+        }
+    }
+
+    
+}
---
+++ AAX_AzureQueueLibrary/SendAzureQueue.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+
+namespace AAX_AzureQueueLibrary
+{
+    public class SendAzureQueue
+    {
+        QueueClient queueClient;
+        BrokeredMessage message;
+
+        public void connect(string connectionString)
+        {
+            queueClient = QueueClient.CreateFromConnectionString(connectionString, ReceiveMode.PeekLock);
+        }
+
+        public void sendMessage(string s)
+        {
+            message = new BrokeredMessage(s);
+            queueClient.Send(message);
+        }
+
+        public void close()
+        {
+            queueClient.Close();
+        }
+    }
+}
